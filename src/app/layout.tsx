71a1ae3f60da0bca9202c9e7d43fe26287131854
Jared Mercer <jaredthebackpacker@gmail.com>
2024-08-10 19:32:07 -0700

    chore: working on auth pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { Montserrat } from "next/font/google";
 import { type Metadata } from "next";
+import Image from "next/image";
+
+import logoImg from "../../public/logo.png";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +14,50 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} bg-black/90 text-white`}>
+      <body className="page-container">
+        <header
+          className={`${montserrat.className} container mx-auto flex items-center justify-between py-4`}
+        >
+          <Link href="/" className="h-fit">
+            <span className="flex items-center gap-x-2 rounded px-2 text-4xl font-semibold hover:bg-white/10">
+              <Image src={logoImg} alt="logo" width={75} height={75} />
+              Triple C Collective CMS
+            </span>
+          </Link>
+          <nav className="flex items-center gap-x-4">
+            <Link href="/auth/login" className="btn">
+              Login
+            </Link>
+            <Link href="/auth/register" className="btn">
+              Register
+            </Link>
+          </nav>
+        </header>
+        <main className="content-wrap">{children}</main>
+        <footer className="footer flex items-center justify-center">
+          <span className="flex text-sm">
+            All Rights Reserved 2023 | Created by
+            <a
+              href="https://jaredthecomputerguy.dev"
+              target="_blank"
+              className="-ml-1"
+            >
+              Jared Mercer
+            </a>
+          </span>
+        </footer>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { getServerAuthSession } from "@/server/auth";
-import { SignInButton, SignOutButton } from "./auth-buttons";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  const thing = await getServerAuthSession();
+  const session = await getServerAuthSession();
 
-  return (
-    <main>
-      Home
-      {thing ? <p>Hello {thing.user.name}</p> : <p>No user</p>}
-      {thing ? <SignOutButton /> : <SignInButton />}
-    </main>
-  );
+  if (!session) {
+    return redirect("/auth/login");
+  }
+
+  return <main>Dashboard</main>;
 }
